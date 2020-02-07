ca850ee95482367b96f637ade49ff090e4e506ab
Gustavo André Fontes Lopes <gflopes@MacBook-Pro-de-Gustavo.local>
2020-02-06 23:46:06 -0300

    Implementação do serviço de cadastro de usuário e autenticação

---
+++ model/usuario.js
@@ -0,0 +1,29 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+const usuarioSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    senha: {
+        type: String,
+        min: 6,
+        max: 12,
+        required: true,
+    },
+    data_criacao: {
+        type: Date,
+        default: Date.now,
+    },
+    data_atualizacao: {
+        type: Date,
+        default: Date.now,
+    },
+    data_ultimo_login: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+module.exports = restful.model('Usuario', usuarioSchema)
